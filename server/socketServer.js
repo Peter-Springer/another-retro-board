491fb7cf4278ca381a5aecf5942d1856d8698b1b
Peter Springer <peterspringer829@gmail.com>
2019-01-05 21:27:57 -0600

    remove gitignore from root

--- server/socketServer.js
+++ server/socketServer.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-const path = require('path')
-app.use(express.static(path.join(__dirname, 'client/build')))
 const server = require('http').createServer(app)
 const io = require('socket.io')(server, { origins: 'http://localhost:3000/'})
 const uuidv4 = require('uuid/v4')
