2c890dc179cea46e573b5975dcf4095ab9ffed12
Peter Springer <peterspringer829@gmail.com>
2019-02-03 16:23:37 -0600

    unable to target correct port from client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-const port = process.env.PORT;
+const port = process.env.NODE_ENV_PORT || 5000;
 const socket = io(`http://localhost:${port}`)
 
 export default socket
