118481fe76c20bdc28d9c116712781562afed760
Peter Springer <peterspringer829@gmail.com>
2019-02-11 21:31:04 -0600

    try to access environment variables from client

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 import io from 'socket.io-client'
 const port = process.env.PORT || 5000;
-const socket = io(`https://anotherretroboard.herokuapp.com:${port}`)
+const socket = io(`http://localhost:${port}`)
+
+console.log(process.env.PORT)
 
 export default socket
