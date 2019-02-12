ea1a007effd2d5dd4fe9df34ef6c19d10c7a9fd4
Peter Springer <peterspringer829@gmail.com>
2019-02-11 20:22:55 -0600

    add server url and port

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
 const port = process.env.PORT || 5000;
-const socket = io(`http://localhost:${port}`)
+const socket = io(`https://anotherretroboard.herokuapp.com:${port}`)
 
 export default socket
